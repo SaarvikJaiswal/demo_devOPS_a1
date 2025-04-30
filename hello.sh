42026b044471accdfb6958ac55820c3a1297344f
Saarvik <saarvikjaiswal@gmail.com>
2025-05-01 01:14:19 +0530

    commit by b2-branch

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 echo "First line added while creating"
-echo "Second line added after 1st commit.......b1"
+echo "Second line added after 1st commit.......b1....b2"
 echo "Line added from branch b1"
-
+echo "Line added from branch b2"
 
--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-print("Hello World....b1")
+print("Hello World....b1.....b2")
 print("Line added from branch b1")
+print("Line added from branch b2")
+
